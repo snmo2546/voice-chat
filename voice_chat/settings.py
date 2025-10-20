dcf93950ab37628050957b46a223b7111c50bce6
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2025-10-20 23:09:14 +0800

    feat: add CSRF_TRUSTED_ORIGINS setting

--- voice_chat/settings.py
+++ voice_chat/settings.py
@@ -32,6 +32,8 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if os.getenv('CSRF_TRUSTED_ORIGINS') else []
+
 
 # Application definition
 
